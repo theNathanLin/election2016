112944b4b01371c2bd4ca079cf5d0935684bfeef
tonynlin123 <anl5ef@virginia.edu>
2016-11-29 14:43:03 -0500

    County labels and comments

--- 2016_general_cleaning_code.R
+++ 2016_general_cleaning_code.R
@@ -10,6 +10,8 @@ library(maps)
 library(scales)
 
 setwd("~/Second Year/DS 4559 - Data Science/Final Project/election2016/Data")
+
+#Read the 2016 general election data + FIPS code database
 gen16 <- read.csv("pres16results.csv", header = TRUE, stringsAsFactors = FALSE)
 fips.labels <- read.csv("fips_database.csv", header = FALSE, stringsAsFactors = FALSE, colClasses = "character")
 
@@ -19,10 +21,12 @@ gen16 <- gen16[-c(1,2),]
 
 sapply(gen16,class)
 
+#This county wasn't labeled in the data, so we manually assigned it the county name
 for (i in seq(nrow(gen16))){
   if (gen16$fips[i] == "46102") gen16$county[i] <- "Oglala Lakota County"
 }
 
+#Creating a separate table with just the state-wide results
 gen16.states <- gen16[is.na(gen16$county),]
 gen16.states <- gen16.states[-c(103,104),] #removing state FIPS for Alaska
 gen16.states$county <- NULL
@@ -30,6 +34,7 @@ gen16.states$fips <- NULL
 
 gen16 <- gen16[-is.na(gen16$county)]
 
+#Removing the statewide results from the county data
 for (i in seq(nrow(gen16))) {
   if (nchar(gen16$fips[i])<=2) gen16$fips[i] <- NA
 }
@@ -42,35 +47,49 @@ gen16.2$votes <- NULL
 gen16.2$total_votes <- NULL
 gen16.2$lead <- NULL
 
+#Used the reshape2 package to convert the dataframe from long to wide format
 long16 <- dcast(gen16.2, fips + st ~ cand, value.var = "pct")
 colnames(long16) <- c("fips", "st", "DonaldTrump", "HillaryClinton")
 long16$diff <- long16$DonaldTrump - long16$HillaryClinton
 
+#Added an extra leading 0 for FIPS codes that were 4 digits long (enables the merge later)
 for (i in seq(nrow(long16))) {
   if (nchar(long16$fips[i])==4) long16$fips[i] <- paste("0",long16$fips[i], sep="")
 }
 
 #Cleaning FIPS database
 fips.labels$fips <- paste(fips.labels$V2,fips.labels$V3, sep = "")
+fips.labels$V1 <- NULL
 fips.labels$V2 <- NULL
 fips.labels$V3 <- NULL
 fips.labels$V5 <- NULL
-colnames(fips.labels) <- c("State", "County", "FIPS")
+colnames(fips.labels) <- c("County", "FIPS")
 
-#Map Generation
+#Merge FIPS database with election results
+long16 <- merge(long16, fips.labels, by.x = "fips", by.y = "FIPS", all.x = TRUE)
+
+####Map Generation####
+#Virginia
+#Pull county information
 counties <- counties("VA")
 
+#Merge map file with election results based on FIPS code
 df_merged <- geo_join(counties, long16, "GEOID", "fips")
 
+#Determine color gradient for the map
 pal <- colorNumeric(
   palette = c("blue", "red"),
   domain = df_merged$percent
 )
 
-popup <- paste0("FIPS Code: ", df_merged$GEOID, "<br>", "Trump Differential: ", percent(round(df_merged$diff,2)),
-                "<br>", "Trump Vote: ", percent(round(df_merged$DonaldTrump,2)), 
-                "<br>", "Clinton Vote: ", percent(round(df_merged$HillaryClinton,2)))
+#Determine the content of the pop-up on mouse click
+popup <- paste0("<b>", paste(df_merged$County, df_merged$st, sep = ", "), "</b> <br>",
+                "<b>FIPS Code: </b>", df_merged$GEOID, "<br>", 
+                "<b>Trump Differential: </b>", percent(round(df_merged$diff,2)),
+                "<br>", "<b>Trump Vote: </b>", percent(round(df_merged$DonaldTrump,2)), 
+                "<br>", "<b>Clinton Vote: </b>", percent(round(df_merged$HillaryClinton,2)))
 
+#Generate the interactive map with legend
 leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(data = df_merged, 
@@ -99,7 +118,7 @@ leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(data = df_merged2, 
               fillColor = ~pal(diff), 
-              color = "#b2aeae", # you need to use hex colors
+              color = "#b2aeae",
               fillOpacity = 0.8, 
               weight = 1, 
               smoothFactor = 0.2,
