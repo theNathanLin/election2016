f21ef583571f752e3ca9dad912b967bf94601b38
aton <andrewton@verizon.net>
2016-12-02 14:39:32 -0500

    editing cleaning 2012 data

--- clean_presdata_2016.R
+++ clean_presdata_2016.R
@@ -1,8 +1,10 @@
+#load data
 all_counties_2012 <- read.csv("~/Documents/OneDrive/UVA_2ndyr/DS4559/election2016/Data/2012data/all_counties_2012.csv")
 all_counties_2012 <- subset(all_counties_2012, fips!="fips")
 #case insensitive search for rows containing obama or romney
 all_counties_2012_romney <- all_counties_2012[grepl('romney',all_counties_2012$candidate,ignore.case=TRUE), ] 
 all_counties_2012_obama <- all_counties_2012[grepl('obama',all_counties_2012$candidate,ignore.case=TRUE), ] 
+#aggregate the double-counted fips codes
 all_counties_2012_obama <- aggregate (. ~ fips, data=all_counties_2012_obama, FUN=sum)
 all_counties_2012_romney <- aggregate (. ~ fips, data=all_counties_2012_romney, FUN=sum)
 
@@ -26,6 +28,5 @@ summary(all_counties_2012$candidate)
 
 all_counties_2012 <- data.frame(lapply(all_counties_2012, as.character), stringsAsFactors=TRUE)
 summary(all_counties_2012)
-summary(all_counties_2012$fips)
-aggregate(. ~ cbind(fips, candidate), data=df, FUN=sum)
+#summary(all_counties_2012$fips)
 
