1b9a29cebcc89739ebc60f433c3459ac61d8c415
aton <andrewton@verizon.net>
2016-12-02 14:35:21 -0500

    fixed cleaning code

--- Data/2012data/clean_presdata_2016.R
+++ Data/2012data/clean_presdata_2016.R
@@ -3,9 +3,13 @@ all_counties_2012 <- subset(all_counties_2012, fips!="fips")
 #case insensitive search for rows containing obama or romney
 all_counties_2012_romney <- all_counties_2012[grepl('romney',all_counties_2012$candidate,ignore.case=TRUE), ] 
 all_counties_2012_obama <- all_counties_2012[grepl('obama',all_counties_2012$candidate,ignore.case=TRUE), ] 
+all_counties_2012_obama <- aggregate (. ~ fips, data=all_counties_2012_obama, FUN=sum)
+all_counties_2012_romney <- aggregate (. ~ fips, data=all_counties_2012_romney, FUN=sum)
+
 all_counties_2012 <- rbind(all_counties_2012_romney,all_counties_2012_obama)
+all_counties_2012 <- subset(all_counties_2012, fips !="")
 summary(all_counties_2012)
-summary(all_counties_2012)
+summary(all_counties_2012$fips)
 
 #include only first letter
 all_counties_2012[,3] <- substring(all_counties_2012[,3], 1, 1) 
@@ -21,3 +25,7 @@ all_counties_2012$candidate <- replace(all_counties_2012$candidate, all_counties
 summary(all_counties_2012$candidate)
 
 all_counties_2012 <- data.frame(lapply(all_counties_2012, as.character), stringsAsFactors=TRUE)
+summary(all_counties_2012)
+summary(all_counties_2012$fips)
+aggregate(. ~ cbind(fips, candidate), data=df, FUN=sum)
+
