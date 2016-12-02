9d60a7789a46ae3af86c8893e9190e75579aabfe
aton <andrewton@verizon.net>
2016-12-02 16:29:37 -0500

    editing 2012 data

--- clean_presdata_2016.R
+++ clean_presdata_2016.R
@@ -1,12 +1,11 @@
-#load data
 all_counties_2012 <- read.csv("~/Documents/OneDrive/UVA_2ndyr/DS4559/election2016/Data/2012data/all_counties_2012.csv")
 all_counties_2012 <- subset(all_counties_2012, fips!="fips")
+all_counties_2012[,4] <- as.numeric(all_counties_2012[,4])
 #case insensitive search for rows containing obama or romney
 all_counties_2012_romney <- all_counties_2012[grepl('romney',all_counties_2012$candidate,ignore.case=TRUE), ] 
 all_counties_2012_obama <- all_counties_2012[grepl('obama',all_counties_2012$candidate,ignore.case=TRUE), ] 
-#aggregate the double-counted fips codes
-all_counties_2012_obama <- aggregate (. ~ fips, data=all_counties_2012_obama, FUN=sum)
-all_counties_2012_romney <- aggregate (. ~ fips, data=all_counties_2012_romney, FUN=sum)
+
+#all_counties_2012_romney <- aggregate (. ~ fips, data=all_counties_2012_romney, FUN=sum)
 
 all_counties_2012 <- rbind(all_counties_2012_romney,all_counties_2012_obama)
 all_counties_2012 <- subset(all_counties_2012, fips !="")
@@ -24,9 +23,11 @@ all_counties_2012$candidate <- replace(all_counties_2012$candidate, all_counties
 all_counties_2012$candidate <- replace(all_counties_2012$candidate, all_counties_2012$candidate=="M", "romney")
 all_counties_2012$candidate <- replace(all_counties_2012$candidate, all_counties_2012$candidate=="r", "romney")
 all_counties_2012$candidate <- replace(all_counties_2012$candidate, all_counties_2012$candidate=="R", "romney")
-summary(all_counties_2012$candidate)
+all_counties_2012 <- aggregate (votes ~ fips+candidate+county, data=all_counties_2012, FUN=sum)
+
+summary(all_counties_2012$candidate) 
 
 all_counties_2012 <- data.frame(lapply(all_counties_2012, as.character), stringsAsFactors=TRUE)
 summary(all_counties_2012)
-#summary(all_counties_2012$fips)
+summary(all_counties_2012$fips)
 
