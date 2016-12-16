4b42b27553b736a351a720d2e73816fd7f93d64f
Nathan Lin <anl5ef@virginia.edu>
2016-12-15 21:13:59 -0500

    Comment header

--- Lin_Ton_Syed_FinalElectionProject_ML.R
+++ Lin_Ton_Syed_FinalElectionProject_ML.R
@@ -1,3 +1,7 @@
+#DS 4559 Final Project
+#Exploring the 2016 Election, Machine Learning Code
+#Nathan Lin, Andrew Ton, Mansoor Syed
+
 census <- read.csv("Data/2016dataprimary/county_facts.csv", header = TRUE, stringsAsFactors = TRUE)
 #check for unclean data
 data <- subset(census,select=c("fips", "area_name", "state_abbreviation", "POP060210","PST040210","AGE775214","SEX255214","RHI225214", "RHI325214", "RHI425214", "RHI525214", "RHI725214", "RHI825214", "EDU635213", "EDU685213", "INC110213", "PVY020213"))
