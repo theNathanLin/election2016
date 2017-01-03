820e15ee73efe69f2461517f5247f38c398d1ed5
Nathan Lin <anl5ef@virginia.edu>
2017-01-03 13:20:14 -0500

    incorporated 2012 votes into 2016

--- popular_votes.r
+++ popular_votes.r
@@ -20,19 +20,22 @@ obama_data[,c(5,6,17)] <- as.data.frame(lapply(obama_data[,c(5,6,17)], normalize
 
 trump_results <- read.csv("Data/2016+Trump+results.csv")
 trump_data <- merge(data, trump_results, by = "fips")
-trump_data[,c(5,6,17)] <- as.data.frame(lapply(trump_data[,c(5,6,17)], normalize))
+trump_data <- merge(trump_data, romney_results[,c(2,5)], by.x = 'fips', by.y = 'fips', all.x = TRUE)
+
+trump_data[,c(5:9,11,13,16,17,21,25)] <- as.data.frame(lapply(trump_data[,c(5:9,11,13,16,17,21,25)], normalize))
 
 clinton_results <- read.csv("Data/2016+Clinton+results.csv")
 clinton_data <- merge(data, clinton_results, by = "fips")
 clinton_data[,c(5,6,17)] <- as.data.frame(lapply(clinton_data[,c(5,6,17)], normalize))
 
+set.seed(1)
 library(neuralnet)
 ## Create the models choosing top attributes
 romney_model <- neuralnet(votes ~ median_Income+pop_sqr_mile2010+pop_total_2010+p_bachelors+p_asian+p_black+p_female+p_hisp+pop_65., data = romney_data,hidden = c(4,3))
-
 obama_model <- neuralnet(votes ~ median_Income+pop_sqr_mile2010+pop_total_2010+p_bachelors+p_asian+p_black+p_female+p_hisp+pop_65., data = obama_data,hidden = c(4,3))
-trump_model <- neuralnet(votes ~ median_Income+pop_sqr_mile2010+pop_total_2010+p_bachelors+p_asian+p_black+p_female+p_hisp+pop_65., data = trump_data,hidden = c(4,3))
-clinton_model <- neuralnet(votes ~ median_Income+pop_sqr_mile2010+pop_total_2010+p_bachelors+p_asian+p_black+p_female+p_hisp+pop_65., data = clinton_data,hidden = c(4,3))
+
+trump_model <- neuralnet(votes.x ~ votes.y+median_Income+pop_sqr_mile2010+pop_total_2010+p_bachelors+p_asian+p_black+p_female+p_hisp+pop_65., data = trump_data,hidden = c(10,2))
+clinton_model <- neuralnet(total_votes ~ median_Income+pop_sqr_mile2010+pop_total_2010+p_bachelors+p_asian+p_black+p_female+p_hisp+pop_65., data = clinton_data,hidden = c(4,3))
 
 #Testing the simple model
 romney_model_simple <- neuralnet(votes ~ pop_total_2010, data = romney_data,hidden = c(1))
@@ -48,3 +51,9 @@ predicted_votes <- romney_results$net.result
 plot(romney_data$votes,predicted_votes)
 cor(romney_data$votes,predicted_votes)
 
+#Testing Trump's full model
+plot(trump_model)
+trump_results <- compute(trump_model, trump_data[,c(5:9,11,13,16,17,25)])
+predicted_votes <- trump_results$net.result
+plot(trump_data$votes.x,predicted_votes)
+cor(trump_data$votes.x,predicted_votes)
